aa6879424c2f428270790288031ba1dfebca28d0
daridev <darideveloper@gmail.com>
2024-08-13 12:51:31 -0600

    style (index/services): add ornaments

--- sections/Services.jsx
+++ sections/Services.jsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import Title from '@/components/Title'
 import Particle from '@/components/ornaments/Particle'
 import Flower from '@/components/ornaments/Flower'
@@ -7,70 +6,103 @@ import Service from '@/components/Service'
 import { servicesData } from '@/data/services'
 
 
-export default function Services () {
+export default function Services() {
   return (
     <section
       id="services"
       className={`
         services
-        container
-        max-w-7xl
         mt-24
+        overflow-hidden
+        relative
+        w-full
       `}
     >
 
-      <div 
+      <Flower
         className={`
-          title-wrapper
-          flex
-          items-center
-          justify-center
+          absolute
+          top-0
+          left-0
+          transform
+          scale-x-150
+          -z-10
+          max-w-xs
+        `}
+      />
+
+      <div
+        className={`
+          container
+          max-w-7xl
         `}
       >
-        <Particle 
+        <div
           className={`
-            mb-6
-          `}
-        />
-        <Title
-          className={`
-            mx-4
+            title-wrapper
+            flex
+            items-center
+            justify-center
           `}
         >
-          De mi para ti
-        </Title>
-        <Particle 
+          <Particle
+            className={`
+              mb-6
+            `}
+          />
+          <Title
+            className={`
+              mx-4
+            `}
+          >
+            De mi para ti
+          </Title>
+          <Particle
+            className={`
+              mb-6
+            `}
+          />
+        </div>
+
+        <section
           className={`
-            mb-6
-          `}
-        />
+            services-wrapper
+            grid
+            grid-cols-1 md:grid-cols-3
+            gap-12
+          `}>
+
+          {
+            servicesData.map((service, index) => {
+              const slug = service.title.toLowerCase().replace(' ', '-')
+              return (
+                <Service
+                  key={index}
+                  title={service.title}
+                  imageSrc={`/images/services/${slug}.webp`}
+                  imageAlt={`Mi servicio ${service.title}`}
+                  link={`/services/${slug}`}
+                />
+              )
+
+            })
+          }
+
+
+        </section>
       </div>
 
-      <section 
+      <Flower
         className={`
-          services-wrapper
-          grid
-          grid-cols-1 md:grid-cols-3
-          gap-12
-        `}>
-
-        {
-          servicesData.map((service, index) => {
-            const slug = service.title.toLowerCase().replace(' ', '-')
-            return (
-              <Service 
-                key={index}
-                title={service.title}
-                imageSrc={`/images/services/${slug}.webp`}
-                imageAlt={`Mi servicio ${service.title}`}
-                link={`/services/${slug}`}
-              />
-            )
-
-          })
-        }
-
-      </section>
+          absolute
+          right-0
+          bottom-0
+          transform
+          scale-x-150
+          -z-10
+          max-w-xs
+        `}
+      />
 
     </section>
   )
