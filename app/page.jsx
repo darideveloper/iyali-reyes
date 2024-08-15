f343c326250c2d59e68ddf6247530276d9ac2e47
daridev <darideveloper@gmail.com>
2024-08-15 17:02:03 -0600

    chore (index): reorder sections

--- app/page.jsx
+++ app/page.jsx
@@ -3,9 +3,9 @@ import Coach from "@/sections/Coach"
 import Heals from "@/sections/Heals"
 import Video from "@/sections/Video"
 import Services from "@/sections/Services"
+import Contact from "@/sections/Contact"
 import Workshops from "@/sections/Workshops"
 import Experience from "@/sections/Experience"
-import Contact from "@/sections/Contact"
 import Podcast from "@/sections/Podcast"
 import Episodes from "@/sections/Episodes"
 
@@ -18,9 +18,9 @@ export default function Home() {
       <Heals />
       <Video />
       <Services />
+      <Contact />
       <Workshops />
       <Experience />
-      <Contact />
       <Podcast />
       <Episodes />
     </>
--- sections/Contact.jsx
+++ sections/Contact.jsx
@@ -104,6 +104,7 @@ export default function Contact() {
         grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5
         gap-6
         text-white
+        my-20
       `}
     >
       <div className={`
