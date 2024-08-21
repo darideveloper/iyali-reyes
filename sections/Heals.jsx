a2255a9e206ba4554c81ff447789df3bea24b035
daridev <darideveloper@gmail.com>
2024-08-21 08:34:40 -0600

    feat (Heals): pdf in Cta

--- sections/Heals.jsx
+++ sections/Heals.jsx
@@ -2,6 +2,7 @@ import Title from "@/components/Title"
 import Image from "next/image"
 import Particle from "@/components/ornaments/Particle"
 import Flower from "@/components/ornaments/Flower"
+import Cta from "@/components/Cta"
 
 
 export default function Heals() {
@@ -40,12 +41,22 @@ export default function Heals() {
               leading-relaxed
             `}
           >
-            Coaching Imagen Personal Consciente
+            Coaching 1 a 1 Imagen Personal Consciente
           </span>
         </Title>
         <p>
           Conecta con tu identidad, recuerda tu brillo y la confianza en quién eres, para que expreses tu ser auténtico a través de la ropa.
         </p>
+
+        <Cta 
+          link="/files/brochure-coaching.pdf"
+          text="Conoce más"
+          dark={true}
+          className={`
+            mt-6
+          `}
+          target="_blank"
+        />
 
         <Flower 
           className={`
