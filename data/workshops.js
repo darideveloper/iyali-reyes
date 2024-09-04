d75ef4eefc1b1a2370b3988702aee24beaebf308
daridev <darideveloper@gmail.com>
2024-09-04 11:49:43 -0600

    feat (Workshops): update data and images

--- data/workshops.js
+++ data/workshops.js
@@ -1,38 +1,48 @@
 const textWorkshop1 = `
-  Quiero invitarlas a un evento único y transformador para cerrar agosto. Un espacio diseñado para nosotras, donde podremos enfocarnos en el amor propio y la autoestima.
+  Asiste a un evento único y transformador para cerrar Septiembre. Un espacio diseñado para ti, que estás enfocada en crecer tu amor propio y la autoestima, con un giro inesperado. Te enseñaremos a usar tu ropa como herramienta para anclar ese amor propio, autoestima y seguridad que necesitas. 
 
   ### Esto es lo que haremos:
 
-  * Recepción Self Love: Reconozcámonos en la incomodidad y descubramos el poder de amarnos a nosotras mismas, incluso en los momentos más desafiantes.
-  * Arcoíris Interno: Aprenderemos cómo los colores pueden influir y transformar nuestras emociones.
-  * Meditación del Color: Una práctica guiada para elevar nuestra energía y conectarnos con nuestro yo interior.
-  * Sound Healing: Nos relajaremos y renovaremos nuestro espíritu a través de la sanación con sonido.
+  * Recepción Self Love: Reconócete en la incomodidad y descubre el poder de amarte a ti misma, incluso en los momentos más desafiantes.
+  * Intenciona tus prendas: Poner intenciones en tus prendas pueden ser herramientas para aumentar tu autoestima y seguridad, no sólo te ayudan a verte bien, aprende a intencionarlas.
+  * Sound Healing: Te relajaremos y renovaremos desde el interior hasta tu espíritu a través de la sanación con sonido.
 
   ### Detalles del Evento:
 
   * Hora: 9:00 am - 12:00 pm
-  * Lugar: Shaman, Av. Tamaulipas #136, Condesa
-  * Cupo Limitado: Solo 10 tickets disponibles
+  * Lugar: Apartando tu ticket
+  * Cupo Limitado: Solo 12 tickets disponibles
+
 `
 
 const textWorkshop2 = `
   ### Módulo 1: Amor Propio - El Primer Paso
+  
   * ¿Qué es el amor propio? ¿Y por qué es tan importante?
   * Aprende a aumentar tu autoestima y sentirte bien contigo mismo.
 
   ### Módulo 2: El Síndrome del Impostor
+
   * ¿Qué es el síndrome del impostor?
   * Cómo te limita en tu vida personal y profesional.
   * Aprende a vencerlo y sentirte seguro de tus logros.
 
   ### Módulo 3: Imagen Personal - Cómo te Ves te Tratan y te Tratas
+
   * La imagen personal y por qué es más que solo apariencia.
   * Lo que compone tu imagen y ¿Cómo te presentas al mundo?
 
   ### Módulo 4: Creencias Limitantes - Supera lo que Te Detiene a verte como siempre has querido
+
   * Identifica tus creencias limitantes sobre tu imagen
   * Cómo estas creencias afectan cómo te ves a ti mismo.
   * Cambia esas creencias y empieza a verte de manera más positiva y poderosa.
+  
+  ### Detalles del Evento:
+
+  * Fecha: 17 de Septiembre 2024
+  * Hora: 20:00 - 21:00 hrs
+  * Modalidad Online
 
 `
 
